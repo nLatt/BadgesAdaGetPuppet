eee5c29f3e90fe7b0e572162a7cd6d09091c4686
Nicolas <6582485n@gmail.com>
2020-09-11 11:16:47 +0200

    forgot to git add the renamed file

---
+++ badge_url_getter.js
@@ -0,0 +1,56 @@
+const puppeteer = require("puppeteer");
+const cluster = require("puppeteer-cluster");
+const dotenv = require("dotenv");
+const fs = require("fs");
+dotenv.config();
+
+(async () => {
+  const website = "http://vibe.adatechschool.fr/?page_id=111";
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowmo: 200,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(website);
+  await page.$eval("#username-107", (el, value) => el.value = value, process.env.User);
+  await page.$eval("#user_password-107", (el, value) => el.value = value, process.env.PASSWORD);
+  page.click("[class=um-button]");
+
+
+  await page.waitForXPath("//li[@class='user-has-not-earned']/a[@href]");
+  let links = await page.$x("//li[@class='user-has-not-earned']/a[@href]");
+
+  const requirement_urls = await page.evaluate((...links) => {
+    return links.map(e => e.href);
+  }, ...links);
+  await write_urls(requirement_urls);
+})();
+
+
+
+async function get_badge() {
+  read_urls();
+};
+
+
+function write_urls(data) {
+  if (data.constructor === Array ) {
+    fs.writeFile("generated_files/requirement_urls.csv", data, function(err) {
+      if (err) throw err;
+      console.log("File saved!");
+    });
+  };
+  return
+};
+
+function read_urls() {
+  urls = fs.readFile("requirement_urls.csv", function(err) {
+    if (err) throw err;
+    console.log("File read!");
+  });
+  console.log(urls, "should work")
+  return urls
+};
